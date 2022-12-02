671f5965e52754026f6195739b4caf3a438adec8
Scott-Gauthier <gauthier.scott@gmail.com>
2022-12-02 15:55:04 -0800

    fixed more bugs.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,24 +14,28 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  const result = await openDB('jate', 1)
-    .transaction('jate', 'readwrite')
-    .objectStore('jate')
-    .put({ id: 1, value: content });
+  console.log('Posted to the indexDB');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: 1, value: content });
+  const result = await request;
   await result
-    ? console.log('getDb implemented', await result.value)
-    : console.log('putDb not implemented', await result.value);
+    ? console.log('putDb implemented', result.value)
+    : console.log('putDb not implemented');
   return result?.value;
 };
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  const result = await openDB('jate', 1)
-    .transaction('jate', 'readonly')
-    .objectStore('jate')
-    .get(1);
+  console.log('GET all from the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
+  const result = await request;
   await result
-    ? console.log('getDb implemented', await result.value)
+    ? console.log('getDb implemented', result.value)
     : console.log('getDb not implemented');
   return result?.value;
 };
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,8 +1,8 @@
 const butInstall = document.getElementById('buttonInstall');
-
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
     window.deferredPrompt = event;
     butInstall.classList.toggle('hidden', false);
 });
@@ -10,7 +10,6 @@ window.addEventListener('beforeinstallprompt', (event) => {
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
     const promptEvent = window.deferredPrompt;
-
     if (!promptEvent) {
       return;
     }
